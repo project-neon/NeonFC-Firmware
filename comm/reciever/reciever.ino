2ca002723722bdd9f1099165bfe7447e530f7830
marcusholanda <marcus.vinicius.99@hotmail.com>
2022-04-15 12:48:06 -0300

    added motor control code

--- comm/reciever/reciever.ino
+++ comm/reciever/reciever.ino
@@ -1,6 +1,15 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+//pin definitions
+#define PWMA 19
+#define PWMB 27
+#define A1  5
+#define A2  32
+#define B1  25
+#define B2  26
+#define stby 33
+
 // This is de code for the board that is in robots
 
 typedef struct recieved_message {
@@ -22,9 +31,66 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
   Serial.println();
 }
 
+void motor_R(int speedR){  // se o valor for positivo gira para um lado e se for negativo troca o sentido
+  if(speedR>0){  
+    digitalWrite(A1, 1);
+    digitalWrite(A2, 0);
+  }else{
+    digitalWrite(A1, 0);
+    digitalWrite(A2, 1);
+  }
+  ledcWrite(1,abs( speedR));
+}
+void motor_L(int speedL){
+  if(speedL>0){
+    digitalWrite(B1, 1);
+    digitalWrite(B2, 0);
+  }else{
+    digitalWrite(B1, 0);
+    digitalWrite(B2, 1);
+  }
+  ledcWrite(2,abs( speedL));
+}
+
+
+void motors_control(float Vel_R, float Vel_L) {
+// os if limitam os valores para no minimo 15 e no máximo 255, mantendo o seu sinal
+  if(Vel_R<15 && Vel_R>-15) Vel_R=0; 
+  if(Vel_R>255 ) Vel_R=255;
+  if(Vel_R<-255) Vel_R=-255;  
+
+  if(Vel_L<15 && Vel_L>-15) Vel_L=0;
+  if(Vel_L>255 ) Vel_L=255;
+  if(Vel_L<-255) Vel_L=-255;
+  
+  motor_R(Vel_R); //manda para a funcao motor um valor de -255 a 255, o sinal signifca a direcao
+  motor_L(Vel_L);
+  
+}
  
 void setup() {
   Serial.begin(115200);
+
+// configuração de pinos 
+
+  ledcAttachPin(PWMA, 1);
+  ledcAttachPin(PWMB, 2);
+
+  ledcSetup(1, 80000, 8);
+  ledcSetup(2, 80000, 8);
+
+  pinMode(stby, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(B2, OUTPUT);
+  digitalWrite(stby, 1);
+  digitalWrite(A1, 0);
+  digitalWrite(A2, 0);
+  digitalWrite(B1, 0);
+  digitalWrite(B2, 0);
+
+// configurações comunicação
   
   WiFi.mode(WIFI_STA);
 
@@ -41,6 +107,8 @@ void setup() {
 void loop() {
   float v_r = robot.v_r;
   float v_l = robot.v_l;
+
+  motors_control(v_r,v_l); //aplica os valores para os motores
   
   Serial.print("Id: ");
   Serial.println(robot.id);
