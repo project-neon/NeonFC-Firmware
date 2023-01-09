e05fbb02fcac28d5887fb1632c0844a8fd325e3b
gTorrez <g.ctorres09@gmail.com>
2023-01-09 16:14:31 -0300

    add adaptions to broadcast in sender

--- comm/sender/sender.ino
+++ comm/sender/sender.ino
@@ -6,11 +6,7 @@
 // This is the code for the board that is connected to PC
 
 // MAC Adress de cada uma das placas que receberao comandos
-// uint8_t broadcast[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-// uint8_t broadcastAddress0[] = {0xA4, 0xCF, 0x12, 0x72, 0xB7, 0x20};
-uint8_t broadcastAddress0[] = {0x0C, 0xDC, 0x7E, 0x5E, 0x97, 0x0C};
-uint8_t broadcastAddress3[] = {0x0C, 0xDC, 0x7E, 0x5E, 0xA3, 0xE8};
-uint8_t broadcastAddress9[] = {0x24, 0x6F, 0x28, 0xAD, 0xD4, 0x80};
+uint8_t broadcast_adr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 //==============
 
@@ -22,22 +18,10 @@ int id, count;
 
 //==============
 
-typedef struct{
-    int id;
-    float v_l;
-    float v_a;
-} commands;
-
-commands robot_0;
-commands robot_3;
-commands robot_9;
-
-//==============
 
 esp_now_peer_info_t peerInfo;
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
 
   ESP_ERROR_CHECK(esp_netif_init());
@@ -61,29 +45,14 @@ void setup()
   }
 
 
-  //register first peer
-  memcpy(peerInfo.peer_addr, broadcastAddress0, 6);
+  //
+  memcpy(peerInfo.peer_addr, broadcast_adr, 6);
   if (esp_now_add_peer(&peerInfo) != ESP_OK) 
   {
     Serial.println("Failed to add peer");
     return;
   }
-  
-  // register second peer
-  memcpy(peerInfo.peer_addr, broadcastAddress3, 6);
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
-  {
-    Serial.println("Failed to add peer");
-    return;
-  }
  
-  // register third peer
-  memcpy(peerInfo.peer_addr, broadcastAddress9, 6);
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
-  {
-    Serial.println("Failed to add peer");
-    return;
-  }
 }
 
 //=============
@@ -94,7 +63,6 @@ void loop() {
       strcpy(tempChars, receivedChars);
           // this temporary copy is necessary to protect the original data
           //   because strtok() used in parseData() replaces the commas with \0
-      parseData();
       sendData();
       newData = false;
   }
@@ -136,57 +104,8 @@ void recvWithStartEndMarkers(){
     }
 }
 
-//===============
 
-void parseData(){      // split the data into its parts
-
-    char * strtokIndx; // this is used by strtok() as an index
-
-    strtokIndx = strtok(tempChars, ",");
-    
-    while (strtokIndx != NULL){
-        id = atoi(strtokIndx);
-        
-        if(id == 0){
-          robot_0.id = 0;         
-          strtokIndx = strtok(NULL, ",");     
-          robot_0.v_l = atof(strtokIndx);       
-          strtokIndx = strtok(NULL, ",");         
-          robot_0.v_a = atof(strtokIndx);
-          strtokIndx = strtok(NULL, ","); 
-          }
-  
-        else if(id == 3){
-          robot_3.id = 3;         
-          strtokIndx = strtok(NULL, ",");     
-          robot_3.v_l = atof(strtokIndx);       
-          strtokIndx = strtok(NULL, ",");         
-          robot_3.v_a = atof(strtokIndx);
-          strtokIndx = strtok(NULL, ","); 
-          }
-  
-        else{
-          robot_9.id = 9;         
-          strtokIndx = strtok(NULL, ",");     
-          robot_9.v_l = atof(strtokIndx);       
-          strtokIndx = strtok(NULL, ",");         
-          robot_9.v_a = atof(strtokIndx);
-          strtokIndx = strtok(NULL, ","); 
-          }
-    }
-    
-}
-
-//===============
-
-void sendData()
-{   
+void sendData(){   
     // esse delay é necessário para que os dados sejam enviados corretamente
-    esp_err_t result_0 = esp_now_send(broadcastAddress0, (uint8_t *) &robot_0, sizeof(robot_0));
-    delay(3);
-    esp_err_t result_3 = esp_now_send(broadcastAddress3, (uint8_t *) &robot_3, sizeof(robot_3));
-    delay(3);
-    esp_err_t result_9 = esp_now_send(broadcastAddress9, (uint8_t *) &robot_9, sizeof(robot_9));
-    delay(3);
-
+    esp_err_t receivedChars = esp_now_send(broadcast_adr, (uint8_t *) &receivedChars, sizeof(receivedChars));
 }
