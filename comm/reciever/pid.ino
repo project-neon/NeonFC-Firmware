8d7ea2cc2dd5f762d50055225be60d73bd4c6745
Joao Pedro Leal <jpkleal@gmail.com>
2023-05-30 17:35:57 -0300

    feat(pid)): implement I and D components

--- comm/reciever/pid.ino
+++ comm/reciever/pid.ino
@@ -1,11 +1,19 @@
+float last_error = 0;
+float error_sum = 0;
+
 float pid(float target, float atual){
 	float kp = -0.125;
 	float kd = 0;
 	float ki = 0;
 
 	float error = target - atual;
-
-	float output = error * kp ;
+  error_sum += error;
+ 
+  float P = error * kp;
+  float I = error_sum * ki;
+  float D = (error - last_error) * kd;
+  
+	float output = P+I+D;
 
 	return output;
 }
