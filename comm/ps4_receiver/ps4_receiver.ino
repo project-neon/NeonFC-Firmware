7d67c57276fe3446b65d30ef74b4d84de7272df9
Eduardo Makita <makitayukio1@gmail.com>
2023-10-23 12:27:30 -0300

    feat(ps4): add spin, turbo and improve control functions

--- comm/ps4_receiver/ps4_receiver.ino
+++ comm/ps4_receiver/ps4_receiver.ino
@@ -1,10 +1,11 @@
 //Biblioteca do controle de PS4
 #include <PS4Controller.h>
+#define NOTE_B0  31
 
 //Setup pinos Locomocão Ponte H (TB6612fng)
 #define BOARD_V1 1
 #define BOARD_V2 2
-#define BOARD BOARD_V1
+#define BOARD BOARD_V2
 //pin definitions for board V1
 #if BOARD == BOARD_V1
   #define PWMA 19
@@ -26,7 +27,30 @@
   #error Board not defined!
 #endif
 
+#define MAC_ADDRESS "56:A4:53:A2:01:8F"
+
+int PS4_L =  0;
+int PS4_R2 = 0;
+int PS4_L2 = 0;
+int linear = 0;
+int angular = 0;
+int speed_limit = 90;
+int first_mark;
+int second_mark;
+int third_mark;
+int spinR = 0;
+int spinL = 0;
+int spinSpeed = 150;
+
 void motor_R(int speedR) { // se o valor for positivo gira para um lado e se for negativo troca o sentido
+  if(spinR) {
+    speedR =  spinSpeed;
+  }
+
+  if(spinL) {
+    speedR = -spinSpeed;
+  }
+  
   if (speedR > 0) {
     digitalWrite(A1, 1);
     digitalWrite(A2, 0);
@@ -34,10 +58,19 @@ void motor_R(int speedR) { // se o valor for positivo gira para um lado e se for
     digitalWrite(A1, 0);
     digitalWrite(A2, 1);
   }
-  ledcWrite(1, abs(speedR) );
+
+  ledcWrite(1, abs(speedR));
 }
 
 void motor_L(int speedL) {
+  if(spinR) {
+    speedL = -spinSpeed;
+  }
+
+  if(spinL) {
+    speedL =  spinSpeed;
+  }
+
   if (speedL > 0) {
     digitalWrite(B1, 1);
     digitalWrite(B2, 0);
@@ -45,37 +78,42 @@ void motor_L(int speedL) {
     digitalWrite(B1, 0);
     digitalWrite(B2, 1);
   }
-  ledcWrite(2, abs(speedL) );
+
+  ledcWrite(2, abs(speedL));
 }
 
 void motors_control(int linear, int angular) {
-  angular = pid(angular, - get_theta_speed() );
+  angular += pid(angular, - get_theta_speed());
 
-  if (linear > 0 ) linear = map(linear, 0,  255,  60,  255);
-  if (linear < 0 ) linear = map(linear, 0, -255, -60, -255);
-  
-  float result_R = linear - angular; //ao somar o angular com linear em cada motor conseguimos a ideia de direcao do robo
-  float result_L = linear + angular;  
-  
-  //não envia valores de potencia abaixo de 15, que não são fortes o suficiente para mover o robo
-  if(result_R < 15 && result_R > -15) result_R = 0; 
-  if(result_L < 15 && result_L > -15 ) result_L = 0;
-  //Não permite valores superiores a 255 ou inferiores a -255
-  if(result_R >  255 ) result_R =  255; 
-  if(result_R < -255 ) result_R = -255;
-  if(result_L >  255 ) result_L =  255;
-  if(result_L < -255 ) result_L = -255;  
-  
-  //manda para a funcao motor um valor de -255 a 255, o sinal signifca a direcao  
-  motor_R(result_R); 
-  motor_L(result_L);
+  float Vel_R = 0; //ao somar o angular com linear em cada motor conseguimos a ideia de direcao do robo
+  float Vel_L = 0;
+
+  if ((linear == 0) && (angular != 0)){
+    Vel_L = angular*(0.6);
+    Vel_R = angular*(-1)*0.6;
+  }
+  else if (angular == 0){
+    Vel_L = linear;
+    Vel_R = linear;
+  }
+  else if (angular > 0){
+    Vel_L = linear; //ao somar o angular com linear em cada motor conseguimos a ideia de direcao do robo
+    Vel_R = linear - (angular*0.6);
+  }
+  else if (angular < 0){
+    Vel_L = linear + (angular*0.6);
+    Vel_R = linear;
+  }
+
+  motor_R(Vel_R); //manda para a funcao motor um valor de -255 a 255, o sinal signifca a direcao
+  motor_L(Vel_L);
 }
 
 void setup(void) {
   Serial.begin(115200);
   while (!Serial) delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  PS4.begin("b0:05:94:46:2d:7c");
+  PS4.begin(MAC_ADDRESS);
   Serial.println("Ready.");
 
   //configuração de pinos
@@ -104,26 +142,65 @@ void setup(void) {
   mpu_init();
 
   while(PS4.isConnected()!= true) delay(20);
+
+  first_mark = millis();
+  third_mark = millis();
 }
 
 void loop() {
   while(PS4.isConnected()) {
-  
-    if(PS4.LStickY() < -25 || PS4.LStickY() > 25){
-      motors_control((1.8)*PS4.LStickY(),(1.3)*PS4.RStickX());
+    PS4_L =  PS4.LStickX();
+    PS4_R2 = PS4.R2Value();
+    PS4_L2 = PS4.L2Value();
 
-    } else { // Controle sobre valores pequenos devido a problemas na funcao map
-      motors_control(0, 0);
+    second_mark = millis();
 
+    if(second_mark - first_mark > 1000) {
+      speed_limit = 90;
     }
+
+    if(PS4.Square()) {
+      first_mark = millis();
+      speed_limit = 150;
+    }
+
+    if (PS4_L < 15 && PS4_L > -15) PS4_L = 0;
+    if (PS4_R2 < 15) PS4_R2 = 0;
+    if (PS4_L2 < 15) PS4_L2 = 0;
+
+    linear  = map(PS4_R2, 0, 255, 0, speed_limit) - map(PS4_L2, 0, 255, 0, speed_limit);
+    angular = PS4_L;
+
+    if(second_mark - third_mark > 500) {
+      spinR = 0;
+      spinL = 0;
+    }
+
+    if(!spinR && !spinL) {
+      if(PS4.R1()) {
+        third_mark = millis();
+        spinR = 1;
+      }
+
+      if(PS4.L1()) {
+        third_mark = millis();
+        spinL = 1;
+      }
+    }
+
+    motors_control(linear, angular);
+
+    delay(3);
   }
   
   //Failsafe
   if(PS4.isConnected()!= true) {
-  motors_control(0,0);
-  Serial.println("Restart");
-  PS4.end();
-  ESP.restart();
-  delay(20);
+    linear = 0;
+    angular = 0;
+    motors_control(0,0);
+    Serial.println("Restart");
+    PS4.end();
+    ESP.restart();
+    delay(20);
   } 
 }
