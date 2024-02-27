ce7ad1d58912b780b3275d96d6799d0f6434b274
Joao Pedro Leal <jpkleal@gmail.com>
2024-02-27 14:51:56 -0300

    feat(receiver): create linear control

--- comm/reciever/pid.ino
+++ comm/reciever/pid.ino
@@ -1,16 +1,24 @@
-float pid(float target, float atual){
-  float kp = kps[id];
-  float ki = 0;
-  float kd = 0;
+float ang_last, ang_total = 0, 0
+float lin_last, lin_total = 0, 0
 
-  float error = target - atual;
-  error_sum += error;
- 
-  float P = error * kp;
-  float I = error_sum * ki;
-  float D = (error - last_error) * kd;
-  
-  float output = P+I+D;
+float pid_ang(float target){
+    return pid(target, - get_theta_speed(), ang_ks, &ang_last, &ang_total);
+}
 
-  return output;
+float pid_lin(float target){
+    return pid(target, - get_linear_speed(), lin_ks, &lin_last, &lin_total);
+}
+
+float pid(float target, float atual, float *ks, float *last, float *total){
+    float error = target - atual;
+    (*error_sum) += error;
+
+    float P = error * ks[0];
+    float I = error_sum * ks[1];
+    float D = (error - last_error) * ks[3];
+
+    float output = P+I+D;
+    *last = error
+
+    return output;
 }
