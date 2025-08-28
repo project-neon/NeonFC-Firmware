01ac1402b3d25c657bd0df32709575637cf55fd1
Júlio César B. O. Sampaio <59067466+Mega2223@users.noreply.github.com>
2025-08-28 23:17:07 +0000

    control_test

---
+++ comm/pid_tunner/control_test.py
@@ -0,0 +1,58 @@
+import serial
+from time import sleep
+from random import randint
+
+esp32 = serial.Serial('/dev/ttyUSB0', 115200)
+
+def twiddle(k, dk, ksi=.3, target=None):
+    if not target:
+        target = run_pid_test(*k)
+    
+    for i, _ in enumerate(k):
+        k[i] += dk[i]
+        new_error = run_pid_test(*k)
+
+        if new_error < target:
+            target = new_error
+            dk[i] *= 1 + ksi
+
+        else:
+            k[i] -= 2*dk[i]
+            new_error = run_pid_test(*k)
+
+            if new_error < target:
+                target = new_error
+                dk[i] *= 1 + ksi
+
+            else:
+                k[i] += dk[i]
+                dk[i] *= 1 - ksi
+               
+
+    return k, dk, ksi, target
+
+def run_pid_test(kp, ki=0, kd=0):
+    print(f"run_pid_test: kp={kp} ki={ki} kd={kd}")
+    # print(f"<{0},{0},{0},{0},{3},{kp},{ki},{kd},{9},{0},{0},{0}>")
+    esp32.write(f"<{0},{0},{0},{0},{3},{1000*kp},{1000*ki},{1000*kd},{9},{0},{0},{0}>".encode())
+    sleep(7)
+    error = esp32.readline()
+    error = error.decode()
+    print("error = ",error)
+    return abs(float(error)) # TODO pq é abs?
+
+params = [[0], [0.5]]
+while True:
+    command = input("insira um kp, um ki e um kd").strip()
+    n = int(input("\tn: ").strip())    
+
+    for _ in range(n):
+        params = twiddle(*params) # TODO
+        print("-------------------------------------------------------------------------------",
+              "best (kp, ki, kd):", params[0],
+              "\nbest error:", params[3],
+              "\ncurrent (dks): ", params[1])
+
+
+esp32.close()
+
